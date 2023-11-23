dd043215e193a332050314f65af2342ac5982d52
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-11-23 12:07:17 +0200

    update

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -20,15 +20,15 @@ export const RegisterForm = () => {
         <FormContainer onSubmit={handleSubmit}>
             <FormLabel>
                 Username
-                <RegisterFormField type="text" name="name" autocomplete="given-name"/>
+                <RegisterFormField type="text" name="name" autoСomplete="given-name"/>
             </FormLabel>
             <FormLabel>
                 Email
-                <RegisterFormField type="email" name="email" autocomplete="off" />
+                <RegisterFormField type="email" name="email" autoСomplete="off" />
             </FormLabel>
             <FormLabel>
                 Password
-                <RegisterFormField type="password" name="password" autocomplete="off" />
+                <RegisterFormField type="password" name="password" autoСomplete="off" />
             </FormLabel>
             <RegisterBtn type="submit">
                 Register
