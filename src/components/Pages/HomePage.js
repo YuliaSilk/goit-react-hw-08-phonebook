38d1564e48ffb766f3a9f1c732cd77a85927050d
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-11-24 11:18:21 +0200

    Update HomePage.js

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,11 +1,9 @@
 const styles = {
     container: {
-      // height: 1200,
       minHeight: 'calc(100vh - 50px)',
       display: 'flex',
       flexDirection: 'column',
-      // alignItems: 'flex-start',
-      // justifyContent: 'center',
+     
       backgroundImage:`url('https://static.displate.com/1200x857/displate/2023-04-08/904827c689f4a12ebcee81867868f6c8_08a1aa477355156f0f6b2b37affa6aa0.jpg')`,
       // backgroundImage: `url('https://static.displate.com/857x1200/displate/2023-02-26/192931441896229c5d355dfaf481bece_63693a2beb0cc55dff4148104028ebc0.jpg')`
     },
