ec8d44938075b9c3457e78b4d12bd1bc739953b0
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-11-24 11:18:23 +0200

    Update RegisterPage.js

--- src/components/Pages/RegisterPage.js
+++ src/components/Pages/RegisterPage.js
@@ -1,9 +1,17 @@
 import { RegisterForm } from "components/RegisterForm/RegisterForm";
 import { Helmet } from "react-helmet";
 
+const styles = {
+    container: {
+      minHeight: 'calc(100vh - 50px)',
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundImage:`url('https://static.displate.com/1200x857/displate/2023-04-08/904827c689f4a12ebcee81867868f6c8_08a1aa477355156f0f6b2b37affa6aa0.jpg')`,
+    }}
+
 export default function Register() {
     return (
-        <div>
+        <div style={styles.container}>
             <Helmet>
                 <title>Registration</title>
             </Helmet>
